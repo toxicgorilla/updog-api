6a24eff63527aeed877850a3ac3ac1ae1f253639
Steve Hughes <steve.hughes@creative-technologies.co.uk>
2020-09-14 18:59:07 +0100

    Added queue for WhoAndWhen

---
+++ UpDog.Api/WhoAndWhen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UpDog.Api.Api
+{
+    public class WhoAndWhen
+    {
+        public string Who { get; }
+
+        public DateTime When { get; }
+
+        public WhoAndWhen(string who, DateTime when)
+        {
+            Who = who;
+            When = when;
+        }
+    }
+}
