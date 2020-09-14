541f0e9e9e348cee06b42f710eaa228c822abc38
Steve Hughes <steve.hughes@creative-technologies.co.uk>
2020-09-14 18:12:07 +0100

    Initial scaffolding

---
+++ FunctionApp1/UpDog.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace FunctionApp1
+{
+    public static class WhatIsUpdog
+    {
+        [FunctionName("what-is-updog")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest request)
+        {
+            const string responseMessage = "Not much, dog. what's up with you?";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
